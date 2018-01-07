e7b6d9e3d908721d6d91d66b93ece7d69ba1fe93
Cathy Douglass <cdouglass256@gmail.com>
2018-01-06 17:03:15 -0800

    Enum for dot/dash elements

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,38 @@
 use std::process::Command;
 
+pub enum Element {
+    Dot,
+    Dash
+}
+use Element::Dot;
+use Element::Dash;
+
+
 fn chain<I>(message: I) -> Command
-where I: Iterator<Item=bool> {
+where I: Iterator<Item=Element> {
+    // create beep command with no tones
     let mut cmd = Command::new("beep");
-    for symbol in message {
-        if symbol {
-            cmd.arg("-f 220").arg("-l 100").arg("-D 100").arg("-n");
-        } else {
-            cmd.arg("-f 220").arg("-l 300").arg("-D 100").arg("-n");
-        }
+    cmd.arg("-l 0");
+
+    for element in message {
+        add_element(element, &mut cmd);
     }
-    cmd.arg("-l 0"); // suppress beep from final "-n"
+
     cmd
 }
 
+fn add_element(element: Element, cmd: &mut Command) {
+    // append new tone; set frequency and following delay
+    cmd.args(&["-n", "-f 220", "-D 100"]);
+
+    // set length of tone
+    match element {
+        Dot  => { cmd.arg("-l 100"); },
+        Dash => { cmd.arg("-l 300"); },
+    }
+}
+
 fn main() {
-    println!("._.");
-    let message = vec![true, false, true];
+    let message = vec![Dot, Dash, Dot];
     chain(message.into_iter()).spawn().unwrap();
 }
