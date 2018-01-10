050bcebd43f07282b22b383532020656d4e04494
Cathy Douglass <cdouglass256@gmail.com>
2018-01-10 09:18:03 -0800

    Add letters 'w', 'k', 'p'

--- src/main.rs
+++ src/main.rs
@@ -11,10 +11,10 @@ use words::WordGenerator;
 
 const WORD_COUNTS: [usize; 4] = [1, 2, 2, 3];
 const WORD_LENGTHS: [usize; 21] = [1, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 5, 5, 5, 6, 6, 6];
-const CHARS_SO_FAR: [char; 13] = ['e', 't', 'a', 'o', 'i', 'n', 's', 'h', 'r', 'd', 'l', 'u', 'm'];
+const CHARS_SO_FAR: [char; 16] = ['e', 't', 'a', 'o', 'i', 'n', 's', 'h', 'r', 'd', 'l', 'u', 'm', 'w', 'k', 'p'];
 const DOUBLES: [char; 4] = ['a', 'i', 'n', 'm'];
-const TRIPLES: [char; 5] = ['o', 's', 'r', 'd', 'u'];
-const QUARTETS: [char; 2] = ['h', 'l'];
+const TRIPLES: [char; 7] = ['o', 's', 'r', 'd', 'u', 'w', 'k'];
+const QUARTETS: [char; 3] = ['h', 'l', 'p'];
 
 fn quiz(message: &String, stdin: &std::io::Stdin) -> bool {
     let mut passing = true;
